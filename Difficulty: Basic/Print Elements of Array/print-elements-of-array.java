6e5a6ae522f35b6cf10885ce5b336ffd4ac2ed91
Vidyanand Sharma <112870569+Vidyanand8305@users.noreply.github.com>
2026-01-17 19:14:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Elements of Array/print-elements-of-array.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+
+class Solution {
+    void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
