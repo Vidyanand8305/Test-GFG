85419e35a648f93952ac3edefb95f44b773b9afa
V Sharma <112870569+Vidyanand8305@users.noreply.github.com>
2026-01-31 22:44:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Peak element/peak-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int peakElement(int[] arr) {
+        int n = arr.length;
+        int low = 0, high = n - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1;   // move right
+            } else {
+                high = mid;      // move left
+            }
+        }
+        return low; // or high (both same)
+    }
+}
