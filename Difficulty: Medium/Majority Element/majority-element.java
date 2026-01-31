dbd1b68ca8e96635c7bde3a8de87e36ee67a26ab
V Sharma <112870569+Vidyanand8305@users.noreply.github.com>
2026-01-31 22:41:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -0,0 +1,34 @@
+class Solution {
+    int majorityElement(int arr[]) {
+
+        int candidate = 0;
+        int count = 0;
+
+        // Step 1: Find candidate
+        for (int num : arr) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        // Step 2: Verify candidate
+        count = 0;
+        for (int num : arr) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        if (count > arr.length / 2) {
+            return candidate;
+        }
+
+        return -1; // IMPORTANT for GFG
+    }
+}
