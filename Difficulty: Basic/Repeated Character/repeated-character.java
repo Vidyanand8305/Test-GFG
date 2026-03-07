c2be6d8e8c28018e54093c920dd69a4c6e26e7b8
V Sharma <112870569+Vidyanand8305@users.noreply.github.com>
2026-03-07 12:01:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Repeated Character/repeated-character.java
+++ Difficulty: Basic/Repeated Character/repeated-character.java
@@ -3,10 +3,11 @@ import java.util.HashMap;
 
 class Solution {
     char firstRep(String S) {
+        
         HashMap<Character, Integer> map = new HashMap<>();
         for(int i = 0; i < S.length(); i++){
             char ch = S.charAt(i);
-            
+
             if(map.containsKey(ch)){
                 int oldF = map.get(ch);
                 map.put(ch, oldF + 1);
@@ -15,15 +16,14 @@ class Solution {
                 map.put(ch, 1);
             }
         }
-        char result = '#';
         for(int i = 0; i < S.length(); i++){
             char ch = S.charAt(i);
-            
+
             if(map.get(ch) > 1){
-                result = ch;
-                break;
+                return ch;
             }
         }
-        return result;
+
+        return '#';
     }
 }
