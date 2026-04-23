00b77861c6a753962bb592ae33b18d634ad444ed
V Sharma <112870569+Vidyanand8305@users.noreply.github.com>
2026-04-23 14:32:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse Using Stack/reverse-using-stack.java
+++ Difficulty: Easy/Reverse Using Stack/reverse-using-stack.java
@@ -1,16 +1,15 @@
 class Solution {
     public String reverse(String S) {
-        char[] stack = new char[S.length()];
-        int top = -1;
-        for (int i = 0; i < S.length(); i++) {
-            stack[++top] = S.charAt(i);
-        }
-
-        StringBuilder ans = new StringBuilder();
-        while (top >= 0) {
-            ans.append(stack[top--]);
-        }
-
-        return ans.toString();
+         Stack<Character> s = new Stack<>();
+      for(int i = 0; i<S.length();i++){
+          
+          s.push(S.charAt(i));
+      }
+      String ans  = "";
+      while(!s.isEmpty()){
+          ans += s.pop();
+      }
+      return ans;
+        
     }
 }
